8406ef5105cb43624f2017f1b66bd80b2dd1687b
CarloBoschis <37075173+CarloBoschis@users.noreply.github.com>
2022-09-17 09:12:53 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ d3.html("assets/Map.svg").then(function(newDocument){
     clusters.style("opacity",1);
   })
 
+  /*FILTRO ROTTE*/
   const zone = d3.selectAll("#edges, #passages, #streets");
   zone.style("opacity",0);
   function update() {
@@ -30,20 +31,9 @@ d3.html("assets/Map.svg").then(function(newDocument){
 
   d3.selectAll(".checkboxOne").on("change",update);
   update()
-
-
-  /*const zone = d3.selectAll("#groups");
-  const button1 = d3.selectAll(".button-groups");
-  zone.style("opacity",0);
-  button1.on("click", function(){
-    zone.style("opacity",1);
-    button1.classed("button-groups-active", true);
-    button1.classed("button-groups", false);
-  })*/
-
 })
 
-/* Open when someone clicks on the span element */
+/* INIZIO MENU */
 function openNav() {
     document.getElementById("myNav").style.width = "100%";
   }
@@ -52,23 +42,28 @@ function openNav() {
   function closeNav() {
     document.getElementById("myNav").style.width = "0%";
   }
+  /*FINE MENU*/
 
- /* function zoom(event) {
+/*INIZIO ZOOM*/
+ function zoom(event) {
       event.preventDefault();
     
-      scale += event.deltaY * -0.01;
+      scale += event.deltaY * -0.001;
     
       // Restrict scale
-      scale = Math.min(Math.max(1, scale), 4);
+      scale = Math.min(Math.max(0.8, scale), 1.5);
     
       // Apply scale transform
       el.style.transform = `scale(${scale})`;
     }
     
-    let scale = 0.1;
+    let scale = 0.001;
     const el = document.querySelector('#visualization-container');
-    document.body.onwheel = zoom; */
+    document.body.onwheel = zoom;
+    /*FINE ZOOM*/
 
+
+/*INIZIO PARALLASSE*/
     (function() {
       // Add event listener
       document.addEventListener("mousemove", parallax);
@@ -88,3 +83,28 @@ function openNav() {
       }
   
   })();
+  /*FINE PARALLASSE*/
+
+  function openPage(pageName, elmnt, color) {
+    // Hide all elements with class="tabcontent" by default */
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Remove the background color of all tablinks/buttons
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+  
+    // Show the specific tab content
+    document.getElementById(pageName).style.display = "block";
+  
+    // Add the specific color to the button used to open the tab content
+    elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
