51aa9b7b24301eb4c494c8a709195fc721465029
CarloBoschis <37075173+CarloBoschis@users.noreply.github.com>
2022-09-20 16:45:09 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,27 @@
+// Funzione per il menu delle navi
+
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+} 
+
+
 // import SVG
 d3.html("assets/Map.svg").then(function(newDocument){
     const svg = newDocument.querySelector("svg");
@@ -43,6 +67,17 @@ function openNav() {
     document.getElementById("myNav").style.width = "0%";
   }
   /*FINE MENU*/
+
+  /* INIZIO MENU PIANETA */
+function openSys(id) {
+  document.getElementById(id).style.display = "block";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeSys(id) {
+  document.getElementById(id).style.display = "none";
+}
+/*FINE MENU PIANETA*/
 
 /*INIZIO ZOOM*/
  function zoom(event) {
